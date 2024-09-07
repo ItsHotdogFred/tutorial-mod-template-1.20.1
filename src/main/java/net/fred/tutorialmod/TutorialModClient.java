318edcce29950f78fc55e1efe3ef3497403199d0
ItsHotdogFred <75401535+ItsHotdogFred@users.noreply.github.com>
2024-09-08 11:56:59 +1200

    Initial commit

---
+++ src/main/java/net/fred/tutorialmod/TutorialModClient.java
@@ -0,0 +1,8 @@
+package net.fred.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {}
+}

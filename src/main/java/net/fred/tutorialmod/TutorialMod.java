4882874071e60c57e53c104339dd0a9641dc053f
ItsHotdogFred <75401535+ItsHotdogFred@users.noreply.github.com>
2024-09-08 12:31:10 +1200

    Update

--- src/main/java/net/fred/tutorialmod/TutorialMod.java
+++ src/main/java/net/fred/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.fred.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fred.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +13,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
